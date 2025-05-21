a034291ceeb8762a42dba735e69054127035ff55
Tommy Issa <tommyissa@hotmail.com>
2025-05-21 21:25:07 +0200

    Har skapat komponenter för webbplatsens sidor och har lagt dem i routes-fil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AddComponent } from './pages/add/add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'add', component: AddComponent},
+    { path: '' , redirectTo: '/home', pathMatch: 'full'}
+];
